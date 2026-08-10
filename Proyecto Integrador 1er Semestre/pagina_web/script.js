65161995b08761a32b552028c8bffc5fced31f2b
jacastellano03 <jacastellano@puce.edu.ec>
2026-08-10 13:15:45 -0500

    Refactor mostrarProvincias function display logic

--- Proyecto Integrador 1er Semestre/pagina_web/script.js
+++ Proyecto Integrador 1er Semestre/pagina_web/script.js
@@ -1,30 +1,21 @@
 function irFuncionamiento() {
-
     const funcionamiento = document.getElementById('funcionamiento');
 
     funcionamiento.scrollIntoView();
-
 }
 
-
 function mostrarProvincias() {
-
     const provincias = document.getElementById('tablaProvincias');
-
     const boton = document.getElementById('botonProvincias');
 
-    if (provincias.style.display === 'flex') {
-
+    if (provincias.style.display == 'flex') {
         provincias.style.display = 'none';
 
         boton.textContent = 'Mostrar provincias';
 
     } else {
-
         provincias.style.display = 'flex';
-
-        boton.textContent = 'Ocultar provincias';
-
+        boton.textContent = 'Ocultar provincias'
     }
 
 }
